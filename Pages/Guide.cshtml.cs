0ec2a4b0eb16ef796de6bc2de8f7c39a42a20720
Wiwatta Mongkhonchit <cheab@opn.ooo>
2022-04-05 13:23:54 +0700

    Layout guide page

---
+++ Pages/Guide.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace miniapp_dotnet_core_example.Pages;
+
+public class GuideModel : PageModel
+{
+	private readonly ILogger<GuideModel> _logger;
+
+	public GuideModel(ILogger<GuideModel> logger)
+	{
+		_logger = logger;
+	}
+
+	public void OnGet()
+	{
+
+	}
+}
